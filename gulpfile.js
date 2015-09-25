d1378608b06d7702ebad3cc3daae8ccdc6f9e79c
Jens Lind <jens.lind@oddhill.se>
2015-09-25 09:23:02 +0200

    Added gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp')
 var babel = require('gulp-babel')
+var watch = require('gulp-watch')
 
 gulp.task('default', function() {
   return gulp.src('src/*')
+    .pipe(watch('src/*'))
     .pipe(babel())
     .pipe(gulp.dest('./'))
 })
